c74e5ee7b422fa7ce58a7818a9a32bc6b24b625e
Seok93 <gkdltjr@example.com>
2020-09-22 10:11:06 +0900

    연결리스트 기반의 스택 구현 예제

---
+++ DataStructure/6. 스택/예제 코드/2_연결리스트 기반의 스택/소스 파일/ListBaseStackMain.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "ListBaseStack.h"
+
+// 연결리스트 기반의 스택 구현 예제
+int main()
+{
+    // Stack의 생성과 초기화
+    Stack stack;
+    StackInit(&stack);
+
+    // 데이터 넣기
+    SPush(&stack, 1);
+    SPush(&stack, 2);
+    SPush(&stack, 3);
+    SPush(&stack, 4);
+    SPush(&stack, 5);
+
+    //데이터 꺼내기
+    while (!SIsEmpty(&stack))
+        printf("%d ", SPop(&stack));
+
+    return 0;
+}
